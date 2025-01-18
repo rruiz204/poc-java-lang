4d53a1166c14b3f2395ab91b044ff3ffe9803c57
roney ruiz <roneyruizrojas@gmail.com>
2025-01-17 23:05:00 -0600

    learn: loops

---
+++ src/Loops.java
@@ -0,0 +1,11 @@
+package src;
+
+public class Loops {
+  public static void main(String[] args) {
+    int[] numbers = {1, 2, 3 , 4};
+
+    for (int number : numbers) {
+      System.out.println(number);
+    }
+  }
+}
