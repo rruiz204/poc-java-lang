ecb98f19b2b5931aa4bf10a2478d027e297a9c56
roney ruiz <roneyruizrojas@gmail.com>
2025-01-18 23:34:21 -0600

    learn: methods

---
+++ src/Methods.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Methods {
+  public static void main(String[] args) {
+    int result = sum(10, 11);
+    System.out.println(result);
+  }
+
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+}
