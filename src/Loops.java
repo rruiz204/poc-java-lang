34407970de0b30aa76fab79ef1e4ac6bbb0593a6
roney ruiz <roneyruizrojas@gmail.com>
2025-01-30 16:56:17 -0600

    learn: loops

--- src/Loops.java
+++ src/Loops.java
@@ -1,10 +1,17 @@
 package src;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Loops {
   public static void main(String[] args) {
-    int[] numbers = {1, 2, 3 , 4};
+    List<Integer> numbers = new ArrayList<>();
+    numbers.add(124);
+    numbers.add(135);
+    numbers.add(199);
+    numbers.add(100);
 
-    for (int number : numbers) {
+    for (Integer number : numbers) {
       System.out.println(number);
     }
   }
