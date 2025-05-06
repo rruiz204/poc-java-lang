fc1b04f9a211f41252be44637a508cd9a4e24547
roney ruiz <roneyruizrojas@gmail.com>
2025-05-05 19:56:38 -0600

    learn: variables and datatypes

---
+++ src/basics/Variables.java
@@ -0,0 +1,22 @@
+package src.basics;
+
+public class Variables {
+  public static void execute() {
+    System.out.println("=== Variables ===");
+
+    int number = 120;
+    System.out.println(number);
+
+    double pi = 3.14;
+    System.out.println(pi);
+
+    char letter = 'R';
+    System.out.println(letter);
+
+    boolean maybe = true;
+    System.out.println(maybe);
+
+    String word = "amazing";
+    System.out.println(word);
+  }
+}
