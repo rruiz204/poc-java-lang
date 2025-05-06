dcb45be72b1291005308b914bb01d56b5a94bf40
roney ruiz <roneyruizrojas@gmail.com>
2025-05-05 19:01:43 -0600

    chore: remove old files

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
-import src.oop.Object;
-
 public class Main {
   public static void main(String[] args) {
-    Object object = new Object();
-    object.random();
+    System.out.println("hello world!");
   }
 }
--- src/oop/Interfaces.java
+++
@@ -1,5 +0,0 @@
-package src.oop;
-
-public interface Interfaces {
-  void random();
-}
