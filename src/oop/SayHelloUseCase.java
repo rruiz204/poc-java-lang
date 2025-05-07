bce93216a64aea24bfa9d96a738e5bffa4b505c4
roney ruiz <roneyruizrojas@gmail.com>
2025-05-06 21:06:50 -0600

    learn: interfaces and classes

---
+++ src/oop/SayHelloUseCase.java
@@ -0,0 +1,8 @@
+package src.oop;
+
+public class SayHelloUseCase implements UseCase {
+  @Override
+  public void execute() {
+    System.out.println("Hellooooo!!!");
+  };
+}
---
+++ src/oop/UseCase.java
@@ -0,0 +1,5 @@
+package src.oop;
+
+public interface UseCase {
+  void execute();
+}
