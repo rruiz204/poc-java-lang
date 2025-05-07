2255a0f8038f96d28d06ec0bb01d2757fb0b46bc
roney ruiz <roneyruizrojas@gmail.com>
2025-05-06 21:07:02 -0600

    chore: adjust main changes

--- Main.java
+++ Main.java
@@ -1,15 +1,44 @@
 import src.basics.Conditions;
 import src.basics.Loops;
 import src.basics.Variables;
+import src.objects.Day;
+import src.objects.Planet;
 import src.objects.Robot;
+import src.objects.Seed;
+import src.oop.SayHelloUseCase;
+import src.oop.UseCase;
 
 public class Main {
   public static void main(String[] args) {
     Loops.execute();
     Variables.execute();
     Conditions.execute();
-
+    
+    System.out.println("=== Classes ===");
     Robot machete = new Robot("Machete");
     machete.Display();
+
+
+    System.out.println("=== Enums ===");
+    Planet mercury = Planet.MERCURY;
+
+    double result = mercury.surfaceGravity();
+    System.out.println(result);
+
+    Day day = Day.MONDAY;
+
+    if (day == Day.FRIDAY) {
+      System.out.println("today is friday!!!");
+    } else {
+      System.out.println("today isnt friday :(");
+    }
+
+    System.out.println("=== Records ===");
+    Seed seed = new Seed(1, "User Seed");
+    seed.Display();
+
+    System.out.println("=== OOP ===");
+    UseCase sayHelloUseCase = new SayHelloUseCase();
+    sayHelloUseCase.execute();
   }
 }
